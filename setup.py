aae964fbde721c5d6ffedf80a2e5274f4c9f80e0
Antonio Castaldo <antonio.castaldo@phd.unipi.it>
2025-03-03 02:28:24 +0100

    New version with COMET

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mtcleanse",
-    version="0.1.0",
+    version="0.2.0",
     author="Antonio Castaldo",
     author_email="antonio.castaldo@phd.unipi.it",
     description="Machine Translation Corpus Cleaning and Processing",
